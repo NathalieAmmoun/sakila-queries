2864dc916ba216ee4bc777cd4d23fdeae067c6ba
Nathalie Ammoun <78752615+NathalieAmmoun@users.noreply.github.com>
2021-11-04 14:59:09 +0200

    Delete SQL Queries directory

--- SQL Queries/sql_queries.sql
+++
@@ -1,8 +0,0 @@
-SELECT id, type, name, year, institute FROM `degrees`;
-
-SELECT F.facFirstName FROM `faculty` F, `degrees` D
-WHERE D.type = 'MS' AND D.name = 'Computer Science';
-
-DELETE FROM `faculty`;
-
-INSERT INTO `faculty`(`id`, `facFirstName`, `facLastName`, `phone`, `location`, `departemnt`) VALUES ('5','Rania','Safadi','76453122','Beirut', 'Arts and Sciences');
